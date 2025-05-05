8ce1d137647999e4c0a5634a0d791f94b61725df
Ibrahim Badr <85135449+ibrahimbadr95@users.noreply.github.com>
2025-05-05 20:34:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
-
+def read_version():
+    with open("VERSION.txt") as f:
+        return f.read().strip()
+        
 setup(
     name="lib-ml",
-    version="1.0.0",
+    version=read_version(),
     packages=find_packages(),
     install_requires=[
         "nltk",
